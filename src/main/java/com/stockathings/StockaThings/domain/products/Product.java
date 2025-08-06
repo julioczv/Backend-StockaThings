1ea8c687dbbc9c784aca327304649fb1fde9238e
julioczv <zjukiita@gmail.com>
2025-08-06 09:32:35 -0300

    feat: adicionando produtos

---
+++ src/main/java/com/stockathings/StockaThings/domain/products/Product.java
@@ -0,0 +1,37 @@
+package com.stockathings.StockaThings.domain.products;
+
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.math.BigDecimal;
+import java.util.UUID;
+
+@Table(name = "Products") // Nome da nossa tabela no Postgree
+@Entity //Quer dizer que tudo que esta dentro da classe vira uma instancia o entity precisa ter um @Id pra funcionar
+@Getter // gera automaticamente os getters e os setters dentro de nossa classe
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+public class Product {
+
+    @Id // Permite que o proprio banco crie o id, sem precisar passar por parametro
+    @GeneratedValue // esse gera o id pra nos
+    private UUID id;
+
+    private String nameProduct;
+
+    private String description;
+
+    private BigDecimal costPrice;
+
+    private BigDecimal sellingPrice;
+
+    private Integer stockQuantity;
+
+    private String Type;
+
+}
