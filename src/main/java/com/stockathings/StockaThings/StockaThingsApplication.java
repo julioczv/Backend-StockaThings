ac142b66d8e4264e2d00456eb1db4c4f7d748ce4
julioczv <zjukiita@gmail.com>
2025-08-05 21:09:05 -0300

    Meu primeiro commit do StockaThings

---
+++ src/main/java/com/stockathings/StockaThings/StockaThingsApplication.java
@@ -0,0 +1,13 @@
+package com.stockathings.StockaThings;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StockaThingsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StockaThingsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/stockathings/StockaThings/StockaThingsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.stockathings.StockaThings;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StockaThingsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
