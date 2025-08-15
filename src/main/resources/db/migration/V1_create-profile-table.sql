017bb2180e02b00458c8de59d7afe74828547fa3
julioczv <zjukiita@gmail.com>
2025-08-15 14:16:58 -0300

    Fazendo deploy railway

--- src/main/resources/db/migration/V1_create-profile-table.sql
+++
@@ -1,7 +0,0 @@
-/*CREATE EXTENSION IF NOT EXISTS "pgcrypto";
-
-CREATE TABLE profile (
-    id UUID DEFAULT gen_random_uuid() PRIMARY KEY,
-    email VARCHAR(100) NOT NULL,
-    password VARCHAR(40) NOT NULL
-);
